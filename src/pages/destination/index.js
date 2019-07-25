14f151d76ff893dcd6913fe047922c6f768ed13a
AlbenLiu <AlbenLiu.126.com>
2019-07-25 09:59:08 +0800

    目的地

--- src/pages/destination/index.js
+++ src/pages/destination/index.js
@@ -1,35 +1,64 @@
-import React from 'react';
+import React,{Component} from 'react'
+import { Carousel,Input,Icon,Card } from 'antd';
+import './destination.scss'
+const { Meta } = Card;
+function onChange(a, b, c) {
+    console.log(a, b, c);
+  }
+class Destination extends Component{
+    constructor(){
+        super();
 
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux'
-import ActionCreator,{removeAction,changeQtyAction} from '../../store/cartAction'
+        this.state = {
+            mainImg:require("./img/美国2.jpg"),
+        }
+    }
+    render(){
+        return(
+        <div style={{marginBottom:'1.2rem'}}>
+        {/* {header} */}
+            <div className="headerBox">
+                <img src={this.state.mainImg} alt="黄石"/>
+                <Icon type="search" style={{fontSize:'0.5rem',color:'#fff',transform:'rotateY(180deg)',margin:'0.16rem 0 0 0.1rem'}} />
+                <div className="city-container">
+                    <h1>美国</h1>
+                    <span className="tag ng-binding">黄石公园季</span>
+                </div>
+            </div>
+        {/* {{content}} */}
+            <div className="contentBox">
+                <div className="product-types">
+                    <h2>产品分类</h2>
+                    
+                </div>
+            </div>
+        </div>
+        )
+    }
 
-let Destination = (props)=>{
-    console.log('Cart:',props)
-    return <div>Destination</div>
 }
 
 
-Destination = connect((state)=>{
-    return {
-        goodslist:state.goodslist,
-        totalPrice:state.goodslist.reduce((prev,item)=>prev+item.goods_promotion_price*item.qty,0)
-    }
-},(dispatch)=>{
-    // return {
-    //     addAction(goods){
-    //         dispatch(addAction(goods))
-    //     },
-    //     removeAction(id){
-    //         dispatch(removeAction(id))
-    //     },
-    //     changeQtyAction({id,qty}){
-    //         dispatch(changeQtyAction({id,qty}))
-    //     }
-    // }
+// Destination = connect((state)=>{
+//     return {
+//         goodslist:state.goodslist,
+//         totalPrice:state.goodslist.reduce((prev,item)=>prev+item.goods_promotion_price*item.qty,0)
+//     }
+// },(dispatch)=>{
+//     // return {
+//     //     addAction(goods){
+//     //         dispatch(addAction(goods))
+//     //     },
+//     //     removeAction(id){
+//     //         dispatch(removeAction(id))
+//     //     },
+//     //     changeQtyAction({id,qty}){
+//     //         dispatch(changeQtyAction({id,qty}))
+//     //     }
+//     // }
 
-    // 下面的代码，等效于上面的代码
-    return bindActionCreators(ActionCreator,dispatch)
-})(Destination)
+//     // 下面的代码，等效于上面的代码
+//     return bindActionCreators(ActionCreator,dispatch)
+// })(Destination)
 
 export default Destination;
