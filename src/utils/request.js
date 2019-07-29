060ff57fec785336c2d6bd483937cb06d1fb0649
wxj <wxj@qq.com>
2019-07-29 13:58:07 +0800

    login and reg

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,15 +3,15 @@ import axios from 'axios';
 // 基本配置：
 //axios.defaults.baseURL = 'https://www.nanshig.com/mobile/index.php';//?act=index
 let instance = axios.create({
-    baseURL: 'https://www.nanshig.com/mobile'
+    baseURL: 'http://localhost:1995'
 })
 
 export function get(url='',params={}){
     return instance.get(url,params)
-}
+}  
 
-export function post(url='',data={},params={}){
-    return instance.get(url,data,params)
+export function post(url='',data={}){
+    return instance.post(url,data)
 }
 
 export default {
